ade6b697fe5648eb2b26185845eeef35a1742a03
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-12 15:40:40 -0400

    created eventListeners and functions
    eventListeners to handle submitting on the form and deleting on the form, created functions to change the color of the body and h1 and to remove the parentnode when clicking on button. Also changed when submitting to form its changed to an <ol> list and changes <li> text content to have an have an upper case at the beginning

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  let ul = document.querySelector("#tasks");
+  let ol = document.createElement("ol");
+  ol.id = "tasks";
+  ul.parentNode.replaceChild(ol, ul);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildTaskLister(e.target["new-task-description"].value);
+    form.reset();
+  });
 });
+
+function buildTaskLister(lister) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "🗑️";
+  li.textContent = `${lister[0].toUpperCase() + lister.slice(1)} `;
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
+function elementColor() {
+  let h1 = document.querySelector("h1");
+  let body = document.querySelector("body");
+  h1.style.color = "blue";
+  body.style.background = "yellow";
+}
+elementColor();
